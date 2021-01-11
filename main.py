c3b4f3b10b69246878b10e34fb76b3bb317d65ac
jh852018 <jhunt852018@gmail.com>
2021-01-11 18:54:54 -0500

    Making CORS compliant

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route('/')
-def main():
+@app.route('/duck-feed-entry', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def entry():
+    if request.method == 'POST':
+        try:
+            data = request.json
+            dfTime = data['dfTime']
+            dfFood = data['dfFood']
+            dfLocation = data['dfLocation']
+            dfCount = data['dfCount']
+            dfFoodType = data['dfFoodType']
+            dfFoodQty = data['dfFoodQty']
+            return json.dumps({'error': False, 'message': 'Successfully added'})
+        except Exception as e:
+            print(f"Exception while processing request: {e}")
+            return json.dumps({'error': True, 'message': 'Failed to add record', 'description': str(e) })
+
+@app.route('/duck-feed-admin', methods=['GET'])
+def admin():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
